6c298b224fdcab14241056b407d94be7e13ebe45
Samudi2000 <samudiperera2000@gmail.com>
2024-05-18 19:21:37 +0530

    test commit

--- src/main/java/com/example/pos/item/Item.java
+++ src/main/java/com/example/pos/item/Item.java
@@ -1,5 +1,37 @@
 package com.example.pos.item;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+
 public class Item {
 
+    private Long id;
+
+    private String item_name;
+
+    private String description;
+
 }
+
+public item(
+        Long id,
+        String item_name,
+        String description){
+this.id = id;
+this.name = item_name;
+this.description = description
+}
+
+@Override
+public String toString() {
+    return "Student{" +
+            "id=" + id +
+            ", name='" + item_name + '\'' +
+            ", email='" + description + '\'' +
+            '}';
+}
+
+
---
+++ src/main/java/com/example/pos/item/ItemController.java
@@ -0,0 +1,4 @@
+package com.example.pos.item;
+
+public class ItemController {
+}
