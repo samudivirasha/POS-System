f66a371ec07c23699496fdf055639e4d02e725f4
Samudi2000 <samudiperera2000@gmail.com>
2024-05-18 20:30:22 +0530

    test commit

---
+++ src/main/java/com/example/pos/item/ItemService.java
@@ -0,0 +1,25 @@
+package com.example.pos.item;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemService (ItemRepository itemRepository) {this.itemRepository = itemRepository;}
+
+    public List<Item> getItems() {return itemRepository.findAll();}
+
+    public void addNewItem(Item item) {
+        Optional<Item> studentOptional = itemRepository
+                .findItemByName (item.getName());
+        if (itemOptional.isPresent()){
+            throw new IllegalStateException("Name taken");
+        }
+        itemRepository.save(item);
+    }
+}
