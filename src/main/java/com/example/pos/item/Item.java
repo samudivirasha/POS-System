9a08a737f2621d0fef094a4ac4188d2917e7d268
Samudi2000 <samudiperera2000@gmail.com>
2024-05-18 22:00:36 +0530

    remove everything from the item class

--- src/main/java/com/example/pos/item/Item.java
+++ src/main/java/com/example/pos/item/Item.java
@@ -19,50 +19,11 @@ public class Item {
             generator = "item_sequence"
     )
 
-    private Long id;
+    
 
-    private String item_name;
-
-    private String description;
-
+    public Item() {
+    }
 }
 
-public item(
-        Long id,
-        String item_name,
-        String description){
-this.id = id;
-this.name = item_name;
-this.description = description
-}
-
-public void setId(Long id) {
-    this.id = id;
-}
-
-public String getName() {
-    return name;
-}
-
-public void setName(String name) {
-    this.name = name;
-}
-
-public String getDescription() {
-    return email;
-}
-
-public void setDescription(String description) {
-    this.description = email;
-}
-
-@Override
-public String toString() {
-    return "Student{" +
-            "id=" + id +
-            ", name='" + item_name + '\'' +
-            ", email='" + description + '\'' +
-            '}';
-}
 
 
