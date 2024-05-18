a7d0f4cc4d81ed067b251c57cb0002354eaca188
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2024-05-18 21:52:59 +0530

    fixed

--- src/main/java/com/example/pos/item/Item.java
+++ src/main/java/com/example/pos/item/Item.java
@@ -1,5 +1,84 @@
 package com.example.pos.item;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Item {
 
+    @Id
+    @SequenceGenerator(
+            name = "item_sequence",
+            sequenceName = "item_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "item_sequence"
+    )
+
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    public Item() {
+    }
+
+    public Item(Long id, String name, String description, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Item(String name, String description, double price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
