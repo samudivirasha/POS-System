3c25fa9c47f4dddaf48589c3bbcef2a313c9b40d
Samudi2000 <samudiperera2000@gmail.com>
2024-05-19 14:32:23 +0530

    category update #3

---
+++ src/main/java/com/example/pos/category/CatRepository.java
@@ -0,0 +1,15 @@
+package com.example.pos.category;
+
+import com.example.pos.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CatRepository
+    extends JpaRepository <Category, Long> {
+
+    Optional<Category> findCategoryByName (String category_name);
+
+}
